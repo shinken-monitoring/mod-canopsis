2768d395f84d3b1a8b36bba3b0359352299fca1d
David Gil <david.gil.marcos@gmail.com>
2014-09-23 13:39:11 +0200

    Fixed another tuple index out of range
    Another tuple index out of range, the same modification that I did in #7

--- module/module.py
+++ module/module.py
@@ -170,7 +170,7 @@ class Canopsis_broker(BaseModule):
 
         except Exception, err:
             logger.error("[Canopsis] Error: there was an error while trying to create message for host")
-            logger.debug('[Canopsis] {0}: {1}'.format(err))
+            logger.debug('[Canopsis] Error: {0}'.format(err))
             return
 
         if not message:
